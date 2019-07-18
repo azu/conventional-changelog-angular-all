e9169732a41ca6431e3b5db0ed8464bc7747c22d
azu <azuciao@gmail.com>
2019-07-18 11:12:23 +0900

    style: prettier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const Q = require(`q`);
 const conventionalChangelog = require(`./conventional-changelog`);
@@ -6,7 +6,8 @@ const parserOpts = require(`./parser-opts`);
 const recommendedBumpOpts = require(`./conventional-recommended-bump`);
 const writerOpts = require(`./writer-opts`);
 
-module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
-  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
-    return {conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts};
-  });
+module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts]).spread(
+    (conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
+        return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts };
+    }
+);
--- writer-opts.js
+++ writer-opts.js
@@ -1,85 +1,87 @@
-'use strict';
+"use strict";
 
 const compareFunc = require(`compare-func`);
 const Q = require(`q`);
 const readFile = Q.denodeify(require(`fs`).readFile);
 const resolve = require(`path`).resolve;
-const conventionalCommitTypes = require(`conventional-commit-types`)
+const conventionalCommitTypes = require(`conventional-commit-types`);
 
 module.exports = Q.all([
-  readFile(resolve(__dirname, `./templates/template.hbs`), `utf-8`),
-  readFile(resolve(__dirname, `./templates/header.hbs`), `utf-8`),
-  readFile(resolve(__dirname, `./templates/commit.hbs`), `utf-8`),
-  readFile(resolve(__dirname, `./templates/footer.hbs`), `utf-8`)
-])
-.spread((template, header, commit, footer) => {
-  const writerOpts = getWriterOpts();
+    readFile(resolve(__dirname, `./templates/template.hbs`), `utf-8`),
+    readFile(resolve(__dirname, `./templates/header.hbs`), `utf-8`),
+    readFile(resolve(__dirname, `./templates/commit.hbs`), `utf-8`),
+    readFile(resolve(__dirname, `./templates/footer.hbs`), `utf-8`)
+]).spread((template, header, commit, footer) => {
+    const writerOpts = getWriterOpts();
 
-  writerOpts.mainTemplate = template;
-  writerOpts.headerPartial = header;
-  writerOpts.commitPartial = commit;
-  writerOpts.footerPartial = footer;
+    writerOpts.mainTemplate = template;
+    writerOpts.headerPartial = header;
+    writerOpts.commitPartial = commit;
+    writerOpts.footerPartial = footer;
 
-  return writerOpts;
+    return writerOpts;
 });
 
 function getWriterOpts() {
-  return {
-    transform: (commit, context) => {
-      const issues = [];
+    return {
+        transform: (commit, context) => {
+            const issues = [];
 
-      commit.notes.forEach(note => {
-        note.title = `BREAKING CHANGES`;
-      });
+            commit.notes.forEach(note => {
+                note.title = `BREAKING CHANGES`;
+            });
 
-      const matchedType = conventionalCommitTypes.types[commit.type]
-      if (matchedType) {
-        commit.type = matchedType.title
-      } else {
-        return undefined; // ignore other type
-      }
+            const matchedType = conventionalCommitTypes.types[commit.type];
+            if (matchedType) {
+                commit.type = matchedType.title;
+            } else {
+                return undefined; // ignore other type
+            }
 
-      if (commit.scope === `*`) {
-        commit.scope = ``;
-      }
+            if (commit.scope === `*`) {
+                commit.scope = ``;
+            }
 
-      if (typeof commit.hash === `string`) {
-        commit.hash = commit.hash.substring(0, 7);
-      }
+            if (typeof commit.hash === `string`) {
+                commit.hash = commit.hash.substring(0, 7);
+            }
 
-      if (typeof commit.subject === `string`) {
-        let url = context.repository ?
-          `${context.host}/${context.owner}/${context.repository}` :
-          context.repoUrl;
-        if (url) {
-          url = `${url}/issues/`;
-          // Issue URLs.
-          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
-            issues.push(issue);
-            return `[#${issue}](${url}${issue})`;
-          });
-        }
-        if (context.host) {
-          // User URLs.
-          commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9]){0,38})/g, `[@$1](${context.host}/$1)`);
-        }
-      }
+            if (typeof commit.subject === `string`) {
+                let url = context.repository
+                    ? `${context.host}/${context.owner}/${context.repository}`
+                    : context.repoUrl;
+                if (url) {
+                    url = `${url}/issues/`;
+                    // Issue URLs.
+                    commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+                        issues.push(issue);
+                        return `[#${issue}](${url}${issue})`;
+                    });
+                }
+                if (context.host) {
+                    // User URLs.
+                    commit.subject = commit.subject.replace(
+                        /\B@([a-z0-9](?:-?[a-z0-9]){0,38})/g,
+                        `[@$1](${context.host}/$1)`
+                    );
+                }
+            }
 
-      // remove references that already appear in the subject
-      commit.references = commit.references.filter(reference => {
-        if (issues.indexOf(reference.issue) === -1) {
-          return true;
-        }
+            // remove references that already appear in the subject
+            commit.references = commit.references.filter(reference => {
+                if (issues.indexOf(reference.issue) === -1) {
+                    return true;
+                }
 
-        return false;
-      });
+                return false;
+            });
 
-      return commit;
-    },
-    groupBy: `type`,
-    commitGroupsSort: `title`,
-    commitsSort: [`scope`, `subject`],
-    noteGroupsSort: `title`,
-    notesSort: compareFunc
-  };
+            return commit;
+        },
+        groupBy: `type`,
+        commitGroupsSort: `title`,
+        commitsSort: [`scope`, `subject`],
+        noteGroupsSort: `title`,
+        notesSort: compareFunc
+    };
 }
